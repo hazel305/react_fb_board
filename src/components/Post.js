0cd65d9c875fb5b8123f47e0cc7886383d455e24
Hyunny <hazel413@naver.com>
2023-10-13 10:45:49 +0900

    [FEAT]update post 수정기능

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import { doc,deleteDoc } from "firebase/firestore";
+import React, { useState } from 'react';
+import { doc,deleteDoc,updateDoc } from "firebase/firestore";
 import { db } from '../firebase';
 
 export default function Post({postObj,userConfirm}) {
-  console.log(postObj);
+  // console.log(postObj);
+  const [edit, setEdit] = useState(false);
+  const [newPost, setNewPost]= useState(postObj.content);
+
+  const toggleEditMode = ()=>{
+    setEdit((prev)=>!prev);
+  }
+
+  const editChange = (e)=>{
+    // setNewPost(e.target.value);
+    let {target:{value}} =e;
+    setNewPost(value);
+  }
+
+
+  const editSubmit = async(e)=>{
+    e.preventDefault();
+    const newPostRef = doc(db, "posts", postObj.id);
+
+    await updateDoc(newPostRef, {
+      content: newPost
+    });
+    setEdit(false);
+  }
   
   const deletePost = async()=>{
     if(window.confirm('정말 삭제하시겠습니까?')){
@@ -14,12 +37,28 @@ export default function Post({postObj,userConfirm}) {
   
   return (
     <li key={postObj.id}>
-        <h4>{postObj.content}</h4>
-        {userConfirm &&
+      {edit? 
+        (
+          <>
+           <form onSubmit={editSubmit}>
+            <input value={newPost} onChange={editChange} required/>
+            <button>edit Post</button>
+          </form>
+            <button onClick={toggleEditMode}>cancel</button>
+          </>
+       
+        ):(
         <>
-         <button onClick={deletePost}>Delete</button>
-        <button>Edit</button>
-        </>}
+          <h4>{postObj.content}</h4>
+            {userConfirm &&
+              <>
+                <button onClick={deletePost}>Delete</button>
+                <button onClick={toggleEditMode}>Edit</button>
+              </>
+            }
+        </>)
+      }
+       
     </li>
         
   )
